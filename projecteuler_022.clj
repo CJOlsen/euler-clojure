a51ef934478d691e34efcb1bf924d8d13e0d0963
cjolsen <christopherjolsen@gmail.com>
2013-10-03 13:23:44 -0600

    cleaned up 22

--- projecteuler_022.clj
+++ projecteuler_022.clj
@@ -24,36 +24,26 @@
 ;; - add the values for each word, multiply by it's position in the sort
 
 
-(def letter-scores {"a" 1 "b" 2 "c" 3 "d" 4 "e" 5 "f" 6 "g" 7 "h" 8 "i" 9 "j" 10 "k" 11 "l" 12 "m" 13 "n" 14 "o" 15 "p" 16 "q" 17 "r" 18 "s" 19 "t" 20 "u" 21 "v" 22 "w" 23 "x" 24 "y" 25 "z" 26 "" 0})
+(def letter-scores {"a" 1 "b" 2 "c" 3 "d" 4 "e" 5 "f" 6 "g" 7 "h" 8 "i" 9 "j" 10 "k" 11 "l" 12 "m" 13 "n" 14 "o" 15 "p" 16 "q" 17 "r" 18 "s" 19 "t" 20 "u" 21 "v" 22 "w" 23 "x" 24 "y" 25 "z" 26 "" 0 "A" 1 "B" 2 "C" 3 "D" 4 "E" 5 "F" 6 "G" 7 "H" 8 "I" 9 "J" 10 "K" 11 "L" 12 "M" 13 "N" 14 "O" 15 "P" 16 "Q" 17 "R" 18 "S" 19 "T" 20 "U" 21 "V" 22 "W" 23 "X" 24 "Y" 25 "Z" 26})
 
 (defn score-word
   [word]
   (reduce + (map letter-scores (clojure.string/split word #""))))
 
-(def sorted-list
-  (sort the-list))
-
-(time (sort the-list))
-;; "Elapsed time: 11.045469 msecs"
-
 (defn problem-22
   []
-  (loop [words sorted-list
+  (loop [words (sort the-list)
          total 0
          index 1]
     (cond (empty? words) total
-          :else
-          (recur (rest words)
-                 (+ total (* (score-word
-                              (clojure.string/lower-case (first words)))
-                             index))
-                 (inc index)))))
+          :else (recur (rest words)
+                       (+ total (* (score-word (first words))
+                                   index))
+                       (inc index)))))
 
 (time (problem-22))
-;; "Elapsed time: 73.366941 msecs"*
+;; "Elapsed time: 67.776566 msecs"
 ;; 871198282
-;;
-;; * plus 11 msecs from sorting the list earlier
 
 
 

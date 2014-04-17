d013a2dad89af5e4f40d934e9623a061888a3e66
cjolsen <christopherjolsen@gmail.com>
2014-04-17 12:15:29 -0600

    unsuccessful improvement on problem 10

--- projecteuler_010.clj
+++ projecteuler_010.clj
@@ -63,3 +63,74 @@
 (time (prob10 2000000))
 ;; "Elapsed time: 31154.428647 msecs"
 ;; 142913828922
+
+
+
+
+
+;; or how about a prime seive?
+
+
+;; this is a rare occasion of mutating a data structure in place
+(defn sift [upper-limit]
+  (let [s (atom (vec (range 0 (inc upper-limit))))]
+    (swap! s assoc 1 0) ;; 1 is not prime
+    (loop [index 2]
+      ;;(println "outer loop, index:" index)
+      (cond (> index upper-limit)
+            @s
+            (= (@s index) 0)
+            (recur (inc index)) ;; if index value is 0, move to next index
+            :else
+            (do (loop [step (* 2 index)] ;; set all multiples of index to 0
+                  ;;(println "inner loop, step:" step)
+                  (cond (> step upper-limit)
+                        '()
+                        :else
+                        (do (swap! s assoc step 0)
+                            (recur (+ step index))))) ;; inner loop/recur
+                (recur (inc index))))))) ;; outer loop/recur
+
+(defn problem10_b [limit]
+  (reduce + (sift limit)))
+
+(time (problem10_b 2000000))
+
+;; "Elapsed time: 24562.689368 msecs"
+;; 142913828922
+
+;; this is much slower (about 5x) than I expected, not sure why...adding an if
+;; statement before the swap! only makes it slower...
+
+;; maybe without the expense of atoms and swapping?
+
+
+(defn sift_b [upper-limit]
+  (let [s (vec (range 0 (inc upper-limit)))]
+    (loop [seive (assoc s 1 0) ;; 1 is not prime
+           index 2]
+      ;;(println "outer loop, index:" index)
+      (cond (> index upper-limit)
+            seive
+            (= (seive index) 0)
+            (recur seive (inc index)) ;; if index value is 0, move to next index
+            :else
+            (recur (loop [seive2 seive ;; outer loop/recur
+                          step (* 2 index)] ;; set all multiples of index to 0
+                     ;;(println "inner loop, step:" step)
+                     (cond (> step upper-limit)
+                           seive2
+                           :else
+                           (recur (assoc seive2 step 0)
+                                  (+ step index)))) ;; inner loop/recur
+                   (inc index))))))
+
+(defn problem10_c [limit]
+  (reduce + (sift_b limit)))
+
+(time (problem10_c 2000000))
+
+;; "Elapsed time: 27799.237893 msecs"
+;; 142913828922
+
+;; so atoms/swap! aren't what's slowing this down....
